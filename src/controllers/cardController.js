0eeec16d9029def660c4faa275bdfb84af1f2bcf
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2025-04-24 10:20:44 -0300

    feat: implement controller, model and routes for cards

---
+++ src/controllers/cardController.js
@@ -0,0 +1,135 @@
+import CardModel from "../models/cardModel.js";
+
+class CardController {
+  // GET /cartas
+  async getAllCards(req, res) {
+    try {
+      const cartas = await CardModel.findAll();
+      res.json(cartas);
+    } catch (error) {
+      console.error("Erro ao buscar as cartas:", error);
+      res.status(500).json({ error: "Erro ao buscar as cartas" });
+    }
+  }
+
+  // GET /cartas/:id
+  async getCardById(req, res) {
+    try {
+      const { id } = req.params;
+
+      const carta = await CardModel.findById(id);
+
+      if (!carta) {
+        return res.status(404).json({ error: "Carta não encontrada!" });
+      }
+
+      res.json(carta);
+    } catch (error) {
+      console.error("Erro ao buscar carta:", error);
+      res.status(500).json({ error: "Erro ao buscar carta" });
+    }
+  }
+
+  // POST /cartas
+  async createCard(req, res) {
+    try {
+      // Captura os dados do corpo da requisição
+      const {
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId,
+      } = req.body;
+
+      // Verifica se todos os campos de cartas foram fornecidos
+      if (
+        !name ||
+        !rarity ||
+        !attacksPoints ||
+        !defensePoints ||
+        !collectionId
+      ) {
+        return res.status(400).json({
+          error:
+            "Os campos nome, raridade, pontos de ataque, pontos de defesa e id da coleção são obrigatórios",
+        });
+      }
+
+      // Criar a nova carta
+      const novaCarta = await CardModel.create(
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId
+      );
+
+      if (!novaCarta) {
+        return res.status(400).json({ error: "Erro ao criar carta" });
+      }
+
+      res.status(201).json({ message: "Carta criada com sucesso!", novaCarta });
+    } catch (error) {
+      console.error("Erro ao criar carta:", error);
+      res.status(500).json({ error: "Erro ao criar carta" });
+    }
+  }
+
+  // PUT /carta/:id
+  async updateCollection(req, res) {
+    try {
+      const { id } = req.params;
+      const {
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId,
+      } = req.body;
+
+      // Atualizar a carta
+      const updatedCard = await CardModel.update(
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId
+      );
+
+      if (!updatedCard) {
+        return res.status(404).json({ error: "Carta não encontrada" });
+      }
+
+      res.json(updatedCard);
+    } catch (error) {
+      console.error("Erro ao atualizar carta:", error);
+      res.status(500).json({ error: "Erro ao atualizar carta!" });
+    }
+  }
+
+  // DELETE /carta/:id
+  async deleteCollection(req, res) {
+    try {
+      const { id } = req.params;
+
+      // Remover a carta
+      const result = await CardModel.delete(id);
+
+      if (!result) {
+        return res.status(404).json({ error: "Carta não encontrada" });
+      }
+
+      res.status(200).json({ message: "Carta removida com sucesso!" });
+    } catch (error) {
+      console.error("Erro ao remover carta:", error);
+      res.status(500).json({ error: "Erro ao remover carta" });
+    }
+  }
+}
+
+export default new CollectionController();
---
+++ src/models/cardModel.js
@@ -0,0 +1,108 @@
+import prisma from "../../prisma/prisma.js";
+
+class CardModel {
+  // Obter todas as cartas
+  async findAll() {
+    const cartas = await prisma.card.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        collection: true,
+      },
+    });
+
+    // console.log(cartas);
+
+    return cartas;
+  }
+
+  // Obter uma carta pelo ID
+  async findById(id) {
+    const carta = await prisma.card.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        collection: true,
+      },
+    });
+
+    return carta;
+  }
+
+  // Criar uma nova carta
+  async create(
+    name,
+    rarity,
+    attacksPoints,
+    defensePoints,
+    imageUrl,
+    collectionId
+  ) {
+    const novaCarta = await prisma.card.create({
+      data: {
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId: Number(collectionId),
+      },
+    });
+
+    return novaCarta;
+  }
+
+  // Atualizar uma carta
+  async update(
+    name,
+    rarity,
+    attacksPoints,
+    defensePoints,
+    imageUrl,
+    collectionId
+  ) {
+    const carta = await this.findById(id);
+
+    if (!carta) {
+      return null;
+    }
+
+    // Atualize uma carta existente com os novos dados
+    const cartaAtualizada = await prisma.card.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name,
+        rarity,
+        attacksPoints,
+        defensePoints,
+        imageUrl,
+        collectionId: Number(collectionId),
+      },
+    });
+
+    return cartaAtualizada;
+  }
+
+  // Remover uma carta
+  async delete(id) {
+    const carta = await this.findById(id);
+
+    if (!carta) {
+      return null;
+    }
+
+    await prisma.card.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    return true;
+  }
+}
+
+export default new CardModel();
